4561baa73f319321ed49bb129eec9c3d2b4d0cd3
MigVarona <migvaronag@gmail.com>
2024-05-27 19:27:17 +0200

    pr

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "https://backvirtual13.onrender.com";
+const backendUrl = "http://localhost:3000";
 
 const ChatContext = createContext();
 
